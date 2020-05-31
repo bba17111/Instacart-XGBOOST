0e1cf84d3a4e9b69a53c0101ecaf739deaa1041f
bba17111 <64008170+bba17111@users.noreply.github.com>
2020-05-31 17:17:37 +0300

    Update instacart-xgboost-gridsearch-notebook-Copy1.py

--- instacart-xgboost-gridsearch-notebook-Copy1.py
+++ instacart-xgboost-gridsearch-notebook-Copy1.py
@@ -16,7 +16,7 @@ gc.enable()                       # Activate
 
 # connect to kaggle api and download files (zip)
 from kaggle.api.kaggle_api_extended import KaggleApi
-api = KaggleApi({"username":"evangeloskalampokis","key":"227307bd0d825ea8f784cb00dc373357"})
+api = KaggleApi({"username":"bba17111","key":"1ab8abb57bb0300a7a61735992d69fcc"})
 api.authenticate()
 files = api.competition_download_files("Instacart-Market-Basket-Analysis")
 
